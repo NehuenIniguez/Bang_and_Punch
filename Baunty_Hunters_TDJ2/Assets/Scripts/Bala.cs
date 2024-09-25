e2261a5542e34a892931c88ddfd8858aac18719a
NehuenIniguez <164099712+NehuenIniguez@users.noreply.github.com>
2024-09-25 18:39:57 -0300

    Feature/ataques

--- Baunty_Hunters_TDJ2/Assets/Scripts/Bala.cs
+++ Baunty_Hunters_TDJ2/Assets/Scripts/Bala.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Bala : MonoBehaviour
@@ -11,4 +12,14 @@ public class Bala : MonoBehaviour
         {
             transform.Translate(Vector2.right * velocidad * Time.deltaTime);
         }   
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log(other.gameObject.name);
+        if (other.gameObject.CompareTag("Enemigo"))
+        {
+            other.gameObject.transform.GetComponent<Enemigo>().TomarDaño(daño);
+        }
+        Destroy(this.gameObject);
+
+    }
 }
--- Baunty_Hunters_TDJ2/Assets/Scripts/Enemigo.cs
+++ Baunty_Hunters_TDJ2/Assets/Scripts/Enemigo.cs
@@ -1,23 +1,23 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//
-//public class Enemigo : MonoBehaviour
-//{
-//    [SerializeField] private float vida;
-//    //[SerializeField] private GameObject efectoMuerte;
-//
-//    public void TomarDaño (float daño)
-//    {
-//        vida -= daño;
-//        if (vida<=0)
-//        {
-//            muerte();
-//        }
-//    }
-//    
-//    private void muerte()
-//    {
-//        Destroy(gameObject);
-//    }
-//}
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float vida;
+    //[SerializeField] private GameObject efectoMuerte;
+
+    public void TomarDaño (float daño)
+    {
+        vida -= daño;
+        if (vida<=0)
+        {
+            muerte();
+        }
+    }
+    
+    private void muerte()
+    {
+        Destroy(gameObject);
+    }
+}
