e8c42c83a8a07e14a47a231cb0036f140d7b1df1
NehuenIniguez <164099712+NehuenIniguez@users.noreply.github.com>
2024-10-23 18:26:19 -0300

    Fix/APIIIIII

--- Baunty_Hunters_TDJ2/Assets/Scripts/Atack.cs
+++ Baunty_Hunters_TDJ2/Assets/Scripts/Atack.cs
@@ -7,18 +7,21 @@ public class Atack : MonoBehaviour
     [SerializeField] private Transform ControladorGolpe;
     [SerializeField] private float radioGolpe;
     [SerializeField] private float dañoGolpe;
+    private bool esIzquierda = false;
 
     private void Update() 
     {
       
-      if (Input.GetKeyDown(KeyCode.A))
+      if (Input.GetKeyDown(KeyCode.A) && !esIzquierda)
         {    
          transform.Rotate(0, -180, 0);
+         esIzquierda = true;
         // También rotar manualmente el ControladorGolpe y otros objetos
          ControladorGolpe.Rotate(-180, -180, 0); // Esto es solo si el ControladorGolpe necesita rotación manual
         }
-        if(Input.GetKeyDown(KeyCode.D))
+        if(Input.GetKeyDown(KeyCode.D) && esIzquierda)
         {
+            esIzquierda = false;
             transform.Rotate(0, 180, 0);
             ControladorGolpe.Rotate(180, 180, 0);
         }
--- Baunty_Hunters_TDJ2/Assets/Scripts/Movement_and_Atack.cs
+++ Baunty_Hunters_TDJ2/Assets/Scripts/Movement_and_Atack.cs
@@ -11,17 +11,17 @@ public class Movement_and_Atack : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.D))
         {
-            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+           // gameObject.GetComponent<SpriteRenderer>().flipX = false;
             gameObject.transform.Translate(4f* Time.deltaTime,0,0);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            gameObject.GetComponent<SpriteRenderer>().flipX = true;
-            gameObject.transform.Translate(-4f* Time.deltaTime,0,0);
+            //gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            gameObject.transform.Translate(4f* Time.deltaTime,0,0);
         }
         
         
