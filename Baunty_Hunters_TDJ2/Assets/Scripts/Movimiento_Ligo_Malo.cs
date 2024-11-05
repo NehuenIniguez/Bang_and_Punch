fae9d41e67d0bbfb4f6a78f1ecef02c73f04a622
NehuenIniguez <164099712+NehuenIniguez@users.noreply.github.com>
2024-11-05 11:55:25 -0300

    Feature/Retroceso_Versus

--- Baunty_Hunters_TDJ2/Assets/Scripts/Movimiento_Ligo_Malo.cs
+++ Baunty_Hunters_TDJ2/Assets/Scripts/Movimiento_Ligo_Malo.cs
@@ -5,26 +5,44 @@ using UnityEngine;
 public class Movimiento_Ligo_Malo : MonoBehaviour
 {
     public float brinco = 0;
+
+    private Rigidbody2D rb2d;
+    public bool sePuedeMover = true;
+    [SerializeField] private Vector2 velocidadRebte;
+    [SerializeField] private float tiempoRetroceso;
+    private void Start() {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
     
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        //Se agregan teclas y movimientos de las teclas 
-        if (Input.GetKey(KeyCode.D))
-        {
-            gameObject.transform.Translate(8f* Time.deltaTime,0,0);
+       
+       if (sePuedeMover)
+       { 
+         if (Input.GetKey(KeyCode.D))
+         {
+             gameObject.transform.Translate(8f* Time.deltaTime,0,0);
+         }
+         if (Input.GetKey(KeyCode.A))
+         {
+             gameObject.transform.Translate(8f* Time.deltaTime,0,0);
+          }
         }
-        if (Input.GetKey(KeyCode.A))
-        {
-            gameObject.transform.Translate(8f* Time.deltaTime,0,0);
-        }
+        
+    }
+    private void Update() {
         if (Input.GetKeyDown(KeyCode.W) && brinco <2)
         {
             //Hacemos que el salto se sume
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,400f));
+            rb2d.AddForce(new Vector2(0, 400f));
             brinco = brinco +1;
             Debug.Log(brinco);
         }
+    }
+    public void Rebote(Vector2 puntoGolpe)
+    {
+        rb2d.velocity= new Vector2(velocidadRebte.x * puntoGolpe.x, velocidadRebte.y ) ;
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -32,5 +50,22 @@ public class Movimiento_Ligo_Malo : MonoBehaviour
         {
             brinco = 0; // Resetea el contador de saltos al tocar el suelo
         }
+        if (collision.gameObject.CompareTag("Bala") || collision.gameObject.CompareTag("Enemigo")) 
+        {
+            // Calcula la dirección del retroceso
+            Vector2 direccionRetroceso = (transform.position - collision.transform.position).normalized;
+            rb2d.AddForce(direccionRetroceso * velocidadRebte.x, ForceMode2D.Impulse);
+
+            // Desactiva el movimiento y espera un tiempo
+            StartCoroutine(DesactivarMovimiento());
+        }
     }
+
+    private IEnumerator DesactivarMovimiento()
+    {
+        sePuedeMover = false;
+        yield return new WaitForSeconds(tiempoRetroceso);
+        sePuedeMover = true;
+    }
+    
 }
--- Baunty_Hunters_TDJ2/Assets/Scripts/Movimiento_y_ataque.cs
+++ Baunty_Hunters_TDJ2/Assets/Scripts/Movimiento_y_ataque.cs
@@ -11,44 +11,50 @@ public class Movimiento_y_ataque : MonoBehaviour
     private Animator animator;
     private AudioSource audioSource;
     [SerializeField] private AudioClip saltarin;
-
+    private Rigidbody2D rb2d;
+    public bool sePuedeMover = true;
+    [SerializeField] private Vector2 velocidadRebte;
+    [SerializeField] private float tiempoRetroceso;
     
     public float salto = 1;
      private void Start() {
         animator = GetComponent<Animator>();
         audioSource = GetComponent<AudioSource>();
+        rb2d = GetComponent<Rigidbody2D>();
      }
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKey("right"))
+        if (sePuedeMover)
         {
-            gameObject.transform.Translate(8f* Time.deltaTime,0,0);
-            animator.SetBool("Derecha", true );   
-        }
+            if (Input.GetKey("right"))
+            {
+                gameObject.transform.Translate(8f* Time.deltaTime,0,0);
+                animator.SetBool("Derecha", true );   
+            }
 
-        if (Input.GetKey("left"))
-        {
-            gameObject.transform.Translate(8f* Time.deltaTime,0,0);
-            animator.SetBool("Derecha", true );
-        }
+            if (Input.GetKey("left"))
+            {
+                gameObject.transform.Translate(8f* Time.deltaTime,0,0);
+                animator.SetBool("Derecha", true );
+            }
 
-        if (Input.GetKeyUp("right"))
-        {   
-            animator.SetBool("Derecha", false);
-        }
+            if (Input.GetKeyUp("right"))
+            {   
+                animator.SetBool("Derecha", false);
+            }
 
-        if( Input.GetKeyUp("left"))
-        {
-            animator.SetBool("Derecha", false);
+            if( Input.GetKeyUp("left"))
+            {
+                animator.SetBool("Derecha", false);
+            }
         }
-        
     }
     private void Update() 
     {
         if (Input.GetKeyDown("up") && salto <2) 
         {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,400f));
+            rb2d.AddForce(new Vector2(0, 400f));
             salto = salto + 1;
             Debug.Log(salto);
             audioSource.PlayOneShot(saltarin);
@@ -58,6 +64,10 @@ public class Movimiento_y_ataque : MonoBehaviour
         {
             animator.SetBool("Salta", false);
         }
+    }
+    public void Rebote(Vector2 puntoGolpe)
+    {
+        rb2d.velocity= new Vector2(velocidadRebte.x * puntoGolpe.x, velocidadRebte.y ) ;
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -69,5 +79,20 @@ public class Movimiento_y_ataque : MonoBehaviour
         {
             SceneManager.LoadScene("GameOverCoop");
         }
+        if (collision.gameObject.CompareTag("Bala") || collision.gameObject.CompareTag("Enemigo")) // Reemplaza "ObjetoEspecifico" con el tag del objeto que quieres
+        {
+            // Calcula la dirección del retroceso
+            Vector2 direccionRetroceso = (transform.position - collision.transform.position).normalized;
+            rb2d.AddForce(direccionRetroceso * velocidadRebte.x, ForceMode2D.Impulse);
+
+            // Desactiva el movimiento y espera un tiempo
+            StartCoroutine(DesactivarMovimiento());
+        }
+    }
+        private IEnumerator DesactivarMovimiento()
+    {
+        sePuedeMover = false;
+        yield return new WaitForSeconds(tiempoRetroceso);
+        sePuedeMover = true;
     }
 }
